43d5a1660e40080f6705f8deec6858f399a1c4a7
nutopaa5 <neuvonen.joonas@gmail.com>
2019-02-11 00:04:06 +0200

    Fixes for errors on JPA + H2

--- bookstore/src/main/java/hh/swd20/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -3,6 +3,7 @@ package hh.swd20.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,10 +15,22 @@ public class BookController {
 		@Autowired
 		private BookRepository repository; 
 	
-		 @RequestMapping(value = "/save", method = RequestMethod.POST)
-		    public String save(Book book){
-		        repository.save(book);
-		        return "redirect:index";
-		    }    
-		
+		@RequestMapping(value="/index")
+	    public String bookList(Model model) {	
+	        model.addAttribute("books", repository.findAll());
+	        return "index";
+	    }
+	  
+	    @RequestMapping(value = "/add")
+	    public String addBook(Model model){
+	    	model.addAttribute("book", new Book());
+	        return "addbook";
+	    }     
+	    
+	    @RequestMapping(value = "/save", method = RequestMethod.POST)
+	    public String save(Book book){
+	        repository.save(book);
+	        return "redirect:index";
+	    }    
+
 }
